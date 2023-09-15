8d772a410b7f5d030b20486753a063c1fbea39cd
Zian Cattoul <zian.cattoul@gmail.com>
2023-09-15 11:21:01 +0200

    Delete commented code

--- main.py
+++ main.py
@@ -9,9 +9,7 @@ if __name__ == "__main__":
     player2 = Player("Nicolas")
     player3 = Player("Alexandre")
 
-    # Create a board and add players to it
     board = Board()
     board.players.extend([player1, player2, player3])
 
-    # Start the game
     board.start_game()
--- utils/game.py
+++ utils/game.py
@@ -21,48 +21,35 @@ class Board:
         """
         Starts the game, fills the deck, distributes cards, and simulates turns until players have no cards left.
         """
-        # Check if there are players to start the game
         if not self.players:
             print("No players to start the game.")
             return
 
-        # Create a deck, fill it, and shuffle it
         deck = Deck()
         deck.fill_deck()
         deck.shuffle()
 
-        # Distribute the cards to the players
         deck.distribute(self.players)
 
-        # Main game loop
         while True:
-            # Increment turn count
             self.turn_count += 1
             print(f"Turn {self.turn_count}")
 
-            # Initialize a list to store cards played in this turn
             cards_played_this_turn = []
-
-            # Each player plays one card
             for player in self.players:
                 if not player.cards:
-                    continue  # Skip players with no cards left
+                    continue
 
                 card_played = player.play()
-                self.active_cards.append(
-                    (player.name, str(card_played))
-                )  # Store player name and card as strings
+                self.active_cards.append((player.name, str(card_played)))
                 cards_played_this_turn.append(card_played)
 
-                # Print the name of the player and the card they played
                 print(f"{player.name} played: {card_played}")
 
-            # Extend history_cards with cards played in this turn
             self.history_cards.extend(cards_played_this_turn)
 
             print("Number of Cards in History:", len(self.history_cards))
 
-            # Check if all players have no cards left
             if all(not player.cards for player in self.players):
                 print("Game Over")
                 break
